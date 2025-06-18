a90573acdc6b2f0105a84db9e2c7ecba750142e0
Gknightt <rivovebayo@gmail.com>
2025-06-18 21:24:36 +0800

    celery in user service config, from workapi to user_service

--- user_service/user_service/celery.py
+++ user_service/user_service/celery.py
@@ -1,8 +1,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workflow_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'user_service.settings')
 
-app = Celery('workflow_api')
+app = Celery('user_service')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
