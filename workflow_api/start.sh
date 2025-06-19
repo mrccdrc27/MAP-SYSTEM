7773bfe10165cfd8b3a90ef709840771b878cb06
cubecore27 <cubecore27@gmail.com>
2025-06-20 02:40:27 +0800

    feat: Update start script to include migrations and seed workflows

--- workflow_api/start.sh
+++ workflow_api/start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # Apply migrations
+python manage.py makemigrations
+python manage.py seed_workflows
 python manage.py migrate
 
 # Start Gunicorn server
-gunicorn user_service.wsgi:application --bind 0.0.0.0:8000
+gunicorn workflow_api.wsgi:application --bind 0.0.0.0:8000
