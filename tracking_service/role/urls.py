fb5df60b0bb52fb7fdde46bf1f6db1a502fc7647
mrccdric27 <cubecore27@gmail.com>
2025-06-24 23:02:52 +0800

    as

--- tracking_service/role/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', PositionListCreateView.as_view(), name='position-list-create'),
-    path('<int:id>/', PositionDetailView.as_view(), name='position-detail-view'),
-]
