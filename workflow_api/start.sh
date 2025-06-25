29c3e60a6956b995208559b8113525332d1b4989
cubecore27 <cubecore27@gmail.com>
2025-06-26 05:15:39 +0800

    feat: Add role seeding command and integrate Celery task for ticket status updates

--- workflow_api/start.sh
+++ workflow_api/start.sh
@@ -4,6 +4,10 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
+python manage.py flush --no-input
+python manage.py seed_roles
+python manage.py seed_workflows
+
 
 # Start Gunicorn server
 gunicorn workflow_api.wsgi:application --bind 0.0.0.0:8000
