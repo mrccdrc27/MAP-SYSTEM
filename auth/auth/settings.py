a98c6cb3fddfc8d38ddf5cd96588f50854043585
adi <125032843+lucky7fever@users.noreply.github.com>
2026-01-10 06:43:05 +0800

    (refactor): Updated urls

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -192,7 +192,7 @@ JWT_SIGNING_KEY = config(
 )
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),  # Short-lived for security, refresh via polling
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),  # Short-lived for security, refresh via polling
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
